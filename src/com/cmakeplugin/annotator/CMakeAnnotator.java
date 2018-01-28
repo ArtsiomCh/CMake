16c461ea4182899abfc46d2c9a82b80bc8796345
Artsiom Chapialiou <achapialiou@gmail.com>
2018-01-27 21:45:31 -0500

    CLion annotator optimizing

--- src/com/cmakeplugin/annotator/CMakeAnnotator.java
+++ src/com/cmakeplugin/annotator/CMakeAnnotator.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.NotNull;
 
 import com.jetbrains.cidr.cpp.cmake.psi.*;
 //import static CMakeKeywords.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,18 +24,22 @@ public class CMakeAnnotator implements Annotator {
     } else if (element instanceof CMakeArgument) {
       if (((CMakeArgument) element).getBracketArgStart()==null) {
         PsiElement cmakeLiteral = ((CMakeArgument) element).getCMakeLiteral();
+        assert cmakeLiteral!=null;
         if (element.getFirstChild().getNode().getElementType() == CMakeTokenTypes.QUOTE) {
           // Annotate Quoted argument
-          annotateVariables(cmakeLiteral, holder);
-        } else if (!(annotateLegacy(cmakeLiteral, holder))){
+          annotateVarReferences(cmakeLiteral, holder);
+        } else
           // Annotate Unquoted argument
-          annotatePathURL(cmakeLiteral, holder);
-          annotateVariables(cmakeLiteral, holder);
-          annotateProperty(cmakeLiteral, holder);
-          annotateOperator(cmakeLiteral, holder);
-//          holder.createInfoAnnotation(cmakeLiteral, null)
-//                  .setTextAttributes(DefaultLanguageHighlighterColors.IDENTIFIER);
-        }
+          if ( !(  annotateLegacy(cmakeLiteral, holder)
+                  || annotateProperty(cmakeLiteral, holder)
+                  || annotateVariable(cmakeLiteral, holder)
+                  || annotateOperator(cmakeLiteral, holder) )) {
+            annotateVarReferences(cmakeLiteral, holder);
+            if (!(annotatePathURL(cmakeLiteral, holder))) {
+              holder.createInfoAnnotation(cmakeLiteral, null)
+                      .setTextAttributes(DefaultLanguageHighlighterColors.IDENTIFIER);
+            }
+          }
       }
     }
   }
@@ -48,7 +52,7 @@ public class CMakeAnnotator implements Annotator {
     } else return false;
   }
 
-  private void annotatePathURL(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+  private boolean annotatePathURL(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     if (element.getText().contains("/")) {
 //      TextRange range = new TextRange(element.getTextRange().getStartOffset(),
 //              element.getTextRange().getStartOffset() + element.getTextRange().getLength());
@@ -56,25 +60,48 @@ public class CMakeAnnotator implements Annotator {
       holder.createInfoAnnotation(element, null)
               .setTextAttributes(DefaultLanguageHighlighterColors.DOC_COMMENT_TAG_VALUE);
 //              .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_PATH_URL);
-    }
+      return true;
+    } else return false;
   }
 
-  private void annotateVariables(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+  private boolean annotateVariable(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+    for (String varRegexp : CMakeKeywords.variables_All) {
+      if (element.getText().matches(varRegexp)) {
+        holder.createInfoAnnotation(element, null)
+                .setTextAttributes(DefaultLanguageHighlighterColors.CONSTANT);
+//                  .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_VARIABLE);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private List<TextRange> outerVarRefsList;
+  private List<TextRange> innerVarsList;
+  private List<TextRange> innerENVvarsList;
+
+  private void annotateVarReferences(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     String argtext = element.getText();
     int pos = element.getTextRange().getStartOffset();
+
     parseVarsIntoLists(argtext);
-    for ( TextRange outerVarRange: outerVarsList) {
+
+    for ( TextRange outerVarRange: outerVarRefsList) {
       holder.createInfoAnnotation( outerVarRange.shiftRight(pos), null)
               .setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
 //              .setTextAttributes(CMakeSyntaxHighlighter.VARIABLE);
     }
     for ( TextRange innerVarRange: innerVarsList ) {
+      String innerVar = argtext.substring(innerVarRange.getStartOffset(),innerVarRange.getEndOffset());
+
+//      markVarInit(innerVar, element, holder);
+
       for (String varRegexp: CMakeKeywords.variables_All ) {
-        if (argtext.substring(innerVarRange.getStartOffset(),innerVarRange.getEndOffset())
-                .matches(varRegexp)) {
+        if ( innerVar.matches(varRegexp)) {
           holder.createInfoAnnotation(innerVarRange.shiftRight(pos), null)
                   .setTextAttributes(DefaultLanguageHighlighterColors.CONSTANT);
 //                  .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_VARIABLE);
+          break;
         }
       }
     }
@@ -85,22 +112,29 @@ public class CMakeAnnotator implements Annotator {
           holder.createInfoAnnotation(innerVarRange.shiftRight(pos), null)
                   .setTextAttributes(DefaultLanguageHighlighterColors.CONSTANT);
 //                  .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_VARIABLE);
+          break;
         }
       }
     }
   }
 
-  private ArrayList<TextRange> outerVarsList;
-  private ArrayList<TextRange> innerVarsList;
-  private ArrayList<TextRange> innerENVvarsList;
-
-/** Fill outerVarsList, innerVarsList and innerENVvarsList with indexes
+//  private void markVarInit(String varName, @NotNull PsiElement elementVarHolder, @NotNull AnnotationHolder holder) {
+//    for (String varName : allVarsList) {
+//      if (element.getText().equals(varName)) {
+//        holder.createInfoAnnotation(element, null)
+//                .setTextAttributes(DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+////                  .setTextAttributes(CMakeSyntaxHighlighter.VARIABLE);
+//        return true;
+//      }
+//    }
+//  }
+/** Fill outerVarRefsList, innerVarsList and innerENVvarsList with indexes
  *  of outer and inner Variables inside given text
    * @param   text   the string to search in.
 */
 // TODO Should be more elegant way to implement that.
   private void parseVarsIntoLists(String text) {
-    outerVarsList = new ArrayList<>();
+    outerVarRefsList = new ArrayList<>();
     innerVarsList = new ArrayList<>();
     innerENVvarsList = new ArrayList<>();
     int varLevel = 0, maxVarLevel = Integer.MIN_VALUE;
@@ -128,7 +162,7 @@ public class CMakeAnnotator implements Annotator {
         varLevel--;
       }
       if (varLevel == 0) {
-        outerVarsList.add(new TextRange(outerVarBegin, matcher.end()));
+        outerVarRefsList.add(new TextRange(outerVarBegin, matcher.end()));
       }
     }
   }
@@ -148,30 +182,34 @@ public class CMakeAnnotator implements Annotator {
     }
   }
 
-  private void annotateProperty(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+  private boolean annotateProperty(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     String propertyName = element.getText().toUpperCase();
     for (String varRegexp: CMakeKeywords.properties_All){
       if (propertyName.matches(varRegexp)){
         holder.createInfoAnnotation(element, null)
                 .setTextAttributes(DefaultLanguageHighlighterColors.NUMBER);
 //                .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_PROPERTY);
+        return true;
       }
     }
     for (String varRegexp: CMakeKeywords.properties_Deprecated){
       if (propertyName.matches(varRegexp)) {
         holder.createWarningAnnotation(element, "Deprecated property")
                 .setHighlightType(ProblemHighlightType.LIKE_DEPRECATED);
+        return true;
       }
     }
+    return false;
   }
 
-  private void annotateOperator(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+  private boolean annotateOperator(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
     String operatorName = element.getText().toUpperCase();
     if (CMakeKeywords.operators.contains(operatorName) ) {
       holder.createInfoAnnotation(element, null)
               .setTextAttributes(DefaultLanguageHighlighterColors.METADATA);
 //              .setTextAttributes(CMakeSyntaxHighlighter.CMAKE_OPERATOR);
-    }
+      return true;
+    } else return false;
   }
 
 }
