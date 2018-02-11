31bd6c35a7d7e17ca1fa327e2e56af8d3a6d58ae
Artsiom Chapialiou <achapialiou@gmail.com>
2018-02-10 21:23:13 -0500

    import CMakeAnnotatorUtils class

--- src/com/cmakeplugin/annotator/CMakeCLionAnnotator.java
+++ src/com/cmakeplugin/annotator/CMakeCLionAnnotator.java
@@ -8,27 +8,29 @@ import org.jetbrains.annotations.NotNull;
 
 import com.jetbrains.cidr.cpp.cmake.psi.*;
 
+import static com.cmakeplugin.annotator.CMakeAnnotatorUtils.*;
+
 public class CMakeCLionAnnotator implements Annotator {
 
   @Override
   public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
     if (element instanceof CMakeCommandName) {
-      CMakeAnnotatorUtils.annotateCommand(element, holder);
+      annotateCommand(element, holder);
     } else if (element instanceof CMakeArgument) {
       if (((CMakeArgument) element).getBracketArgStart()==null) {
         PsiElement cmakeLiteral = ((CMakeArgument) element).getCMakeLiteral();
         assert cmakeLiteral!=null;
         if (element.getFirstChild().getNode().getElementType() == CMakeTokenTypes.QUOTE) {
           // Annotate Quoted argument
-          CMakeAnnotatorUtils.annotateVarReferences(cmakeLiteral, holder);
+          annotateVarReferences(cmakeLiteral, holder);
         } else
           // Annotate Unquoted argument
-          if ( !(  CMakeAnnotatorUtils.annotateLegacy(cmakeLiteral, holder)
-                  || CMakeAnnotatorUtils.annotateProperty(cmakeLiteral, holder)
-                  || CMakeAnnotatorUtils.annotateVariable(cmakeLiteral, holder)
-                  || CMakeAnnotatorUtils.annotateOperator(cmakeLiteral, holder) )) {
-            CMakeAnnotatorUtils.annotateVarReferences(cmakeLiteral, holder);
-            if (!(CMakeAnnotatorUtils.annotatePathURL(cmakeLiteral, holder))) {
+          if ( !(  annotateLegacy(cmakeLiteral, holder)
+                  || annotateProperty(cmakeLiteral, holder)
+                  || annotateVariable(cmakeLiteral, holder)
+                  || annotateOperator(cmakeLiteral, holder) )) {
+            annotateVarReferences(cmakeLiteral, holder);
+            if (!(annotatePathURL(cmakeLiteral, holder))) {
               holder.createInfoAnnotation(cmakeLiteral, null)
                       .setTextAttributes(DefaultLanguageHighlighterColors.IDENTIFIER);
             }
--- src/com/cmakeplugin/annotator/CMakeIdeaAnnotator.java
+++ src/com/cmakeplugin/annotator/CMakeIdeaAnnotator.java
@@ -7,28 +7,30 @@ import org.jetbrains.annotations.NotNull;
 
 import com.cmakeplugin.psi.*;
 
+import static com.cmakeplugin.annotator.CMakeAnnotatorUtils.*;
+
 public class CMakeIdeaAnnotator implements Annotator {
 
   @Override
   public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
     if (element instanceof CMakeCommandName) {
-      CMakeAnnotatorUtils.annotateCommand(element, holder);
+      annotateCommand(element, holder);
     } else if (element instanceof CMakeQuotedArgumentContainer) {
       // Annotate Quoted argument
       assert element.getFirstChild() instanceof CMakeBrace && element.getLastChild() instanceof CMakeBrace;
       PsiElement quotedArgument = element.getFirstChild().getNextSibling();
       if (quotedArgument.getNode().getElementType().getClass().isInstance(CMakeTypes.QUOTED_ARGUMENT))
-        CMakeAnnotatorUtils.annotateVarReferences(quotedArgument, holder);
+        annotateVarReferences(quotedArgument, holder);
     } else if (element instanceof CMakeUnquotedArgumentContainer) {
       // Annotate Unquoted argument
       PsiElement unquotedArgument = element.getFirstChild();
       if (unquotedArgument!=null && unquotedArgument.getNode().getElementType().getClass().isInstance(CMakeTypes.UNQUOTED_ARGUMENT)) {
-        if ( !(  CMakeAnnotatorUtils.annotateLegacy(unquotedArgument, holder)
-                || CMakeAnnotatorUtils.annotateProperty(unquotedArgument, holder)
-                || CMakeAnnotatorUtils.annotateVariable(unquotedArgument, holder)
-                || CMakeAnnotatorUtils.annotateOperator(unquotedArgument, holder) )) {
-          CMakeAnnotatorUtils.annotateVarReferences(unquotedArgument, holder);
-          CMakeAnnotatorUtils.annotatePathURL(unquotedArgument, holder);
+        if ( !(  annotateLegacy(unquotedArgument, holder)
+                || annotateProperty(unquotedArgument, holder)
+                || annotateVariable(unquotedArgument, holder)
+                || annotateOperator(unquotedArgument, holder) )) {
+          annotateVarReferences(unquotedArgument, holder);
+          annotatePathURL(unquotedArgument, holder);
         }
       }
     }
