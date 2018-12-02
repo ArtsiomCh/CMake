4eb70ebae34247c14134b80741dcd33f3e01d5bf
Artsiom Chapialiou <achapialiou@gmail.com>
2018-12-02 17:04:15 -0500

    optimisations for StringUtils: CPU (precompiled Patterns) and Mem (pre-allocated emptyList)

--- src/main/java/com/cmakeplugin/utils/CMakeIFWHILEcheck.java
+++ src/main/java/com/cmakeplugin/utils/CMakeIFWHILEcheck.java
@@ -2,6 +2,7 @@ package com.cmakeplugin.utils;
 
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import java.util.Collections;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -10,26 +11,26 @@ public class CMakeIFWHILEcheck {
 
   @NotNull
   public static List<TextRange> getOuterVarRefs(PsiElement element) {
-    List<TextRange> result =  CMakeVarStringUtil.getOuterVarRefs(element.getText());
+    List<TextRange> result = CMakeVarStringUtil.getOuterVarRefs(element.getText());
     if (result.isEmpty() && isVarInsideIFWHILE(element)) {
-      result.add(new TextRange(0,element.getTextLength()));
+      return Collections.singletonList(new TextRange(0, element.getTextLength()));
     }
     return result;
   }
 
   @NotNull
   public static List<TextRange> getInnerVars(PsiElement element) {
-    List<TextRange> result =  CMakeVarStringUtil.getInnerVars(element.getText());
+    List<TextRange> result = CMakeVarStringUtil.getInnerVars(element.getText());
     if (result.isEmpty() && isVarInsideIFWHILE(element)) {
-      result.add(new TextRange(0,element.getTextLength()));
+      return Collections.singletonList(new TextRange(0, element.getTextLength()));
     }
     return result;
   }
 
   public static boolean isVarInsideIFWHILE(PsiElement element) {
     return CMakePDC.isUnquotedArgument(element)
-            && CMakePDC.hasIfWhileParent(element)
-            && !element.getText().matches("[0-9]+") //hack to skip numbers... not really correct
-            && CMakeVarStringUtil.isPossibleVarDefinition(element.getText());
+        && CMakePDC.hasIfWhileParent(element)
+        && !element.getText().matches("[0-9]+") // hack to skip numbers... not really correct
+        && CMakeVarStringUtil.isPossibleVarDefinition(element.getText());
   }
 }
--- src/main/java/com/cmakeplugin/utils/CMakeVarStringUtil.java
+++ src/main/java/com/cmakeplugin/utils/CMakeVarStringUtil.java
@@ -1,6 +1,8 @@
 package com.cmakeplugin.utils;
 
 import com.intellij.openapi.util.TextRange;
+import java.util.Collections;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -9,89 +11,116 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CMakeVarStringUtil {
-  private static final String CMAKE_ESCAPE_SEQUENCE="(\\\\[^A-Za-z0-9;]|\\\\t|\\\\r|\\\\n|\\\\;)";
-  private static final String CMAKE_VAR_NAME="([A-Za-z0-9/_.+-]|"+CMAKE_ESCAPE_SEQUENCE+")+";
-  private static final String CMAKE_VAR_BEGIN="((^\\$|(?<=[^\\\\])\\$)\\{)"; // Escaped \$ excluded
-  private static final String CMAKE_VAR_END="}";
-  private static final String CMAKE_ENV_VAR_BEGIN="(^ENV\\{)";
-  private static final String CMAKE_ENV_VAR_REF_BEGIN="((^\\$|(?<=[^\\\\])\\$)ENV\\{)"; // Escaped \$ excluded
+  private static final String ESCAPE_SEQUENCE = "(\\\\[^A-Za-z0-9;]|\\\\t|\\\\r|\\\\n|\\\\;)";
+  private static final String VAR_NAME = "([A-Za-z0-9/_.+-]|" + ESCAPE_SEQUENCE + ")+";
+  private static final String VAR_BEGIN = "((^\\$|(?<=[^\\\\])\\$)\\{)"; // Escaped \$ excluded
+  private static final String VAR_END = "}";
+  private static final String ENV_VAR_BEGIN = "(^ENV\\{)";
+  private static final String ENV_VAR_REF_BEGIN =
+      "((^\\$|(?<=[^\\\\])\\$)ENV\\{)"; // Escaped \$ excluded
 
-  public static boolean isPossibleVarDefinition(String text) {
-    return text.matches(CMAKE_VAR_NAME);
+  @Contract(pure = true)
+  public static boolean isPossibleVarDefinition(@NotNull String text) {
+    return text.matches(VAR_NAME);
   }
 
+  private static final List<TextRange> EMPTY_RANGES_LIST = Collections.emptyList();
+
+  private static final Pattern patternOuterVarRefs =
+      Pattern.compile(VAR_BEGIN + "|" + ENV_VAR_BEGIN + "|" + ENV_VAR_REF_BEGIN + "|" + VAR_END);
   /**
    * Parse giving text to find outer variables boundaries
+   *
    * @param text
    * @return list of outer variables boundaries including ${ or $ENV{ or ENV{ and }
    */
-// TODO Should be more elegant way to implement that.
   @NotNull
   public static List<TextRange> getOuterVarRefs(String text) {
+    Matcher matcher = patternOuterVarRefs.matcher(text);
+    if (!matcher.find()) return EMPTY_RANGES_LIST;
+
     List<TextRange> result = new ArrayList<>();
-    int varLevel = 0, maxVarLevel = Integer.MIN_VALUE;
+    int varLevel = 0;
+    int maxVarLevel = Integer.MIN_VALUE;
     int outerVarBegin = 0;
-    Pattern pattern = Pattern.compile(
-            CMAKE_VAR_BEGIN+"|"+CMAKE_ENV_VAR_BEGIN+"|"+CMAKE_ENV_VAR_REF_BEGIN+"|"+CMAKE_VAR_END);
-    Matcher matcher = pattern.matcher(text);
-    while (matcher.find()) {
-      if (       matcher.group().matches(CMAKE_VAR_BEGIN)
-              || matcher.group().matches(CMAKE_ENV_VAR_BEGIN)
-              || matcher.group().matches(CMAKE_ENV_VAR_REF_BEGIN) ) {
+    do {
+      if (matcher.group().matches(VAR_BEGIN)
+          || matcher.group().matches(ENV_VAR_BEGIN)
+          || matcher.group().matches(ENV_VAR_REF_BEGIN)) {
         if (varLevel < 1) {
           varLevel = 0;
-          outerVarBegin=matcher.start();
+          outerVarBegin = matcher.start();
         }
         maxVarLevel = ++varLevel;
-      } else if ( matcher.group().matches(CMAKE_VAR_END) ) {
+      } else if (matcher.group().matches(VAR_END)) {
         if (varLevel == maxVarLevel) {
           maxVarLevel = Integer.MIN_VALUE;
         }
         varLevel--;
       }
-      if ((varLevel == 0) && text.substring(outerVarBegin, matcher.end()).matches(
-              // check if allowed cmake variable symbols only used
-              // https://cmake.org/cmake/help/latest/manual/cmake-language.7.html#variable-references
-              "("+CMAKE_VAR_BEGIN+"|"+CMAKE_ENV_VAR_BEGIN+"|"+CMAKE_ENV_VAR_REF_BEGIN+"|"
-              +CMAKE_VAR_NAME+"|"+CMAKE_VAR_END+")+")
-              ) {
+      if ((varLevel == 0)
+          && text.substring(outerVarBegin, matcher.end())
+              .matches(
+                  // check if allowed cmake variable symbols only used
+                  // https://cmake.org/cmake/help/latest/manual/cmake-language.7.html#variable-references
+                  "("
+                      + VAR_BEGIN
+                      + "|"
+                      + ENV_VAR_BEGIN
+                      + "|"
+                      + ENV_VAR_REF_BEGIN
+                      + "|"
+                      + VAR_NAME
+                      + "|"
+                      + VAR_END
+                      + ")+")) {
         result.add(new TextRange(outerVarBegin, matcher.end()));
       }
-    }
+    } while (matcher.find());
     return result;
   }
 
+  private static final Pattern patternInnerVars =
+      Pattern.compile("(?<=" + VAR_BEGIN + ")" + VAR_NAME + "(?=" + VAR_END + ")");
   /**
    * Parse giving text to find inner variables boundaries
+   *
    * @param text
    * @return list of inner variables boundaries NOT including ${ and }
    */
   @NotNull
   public static List<TextRange> getInnerVars(String text) {
-    List<TextRange> result = new ArrayList<>();
-    Pattern pattern = Pattern.compile("(?<="+ CMAKE_VAR_BEGIN +")"+ CMAKE_VAR_NAME +"(?="+CMAKE_VAR_END+")");
-    Matcher matcher = pattern.matcher(text);
-    while (matcher.find()) {
-      result.add(new TextRange(matcher.start(), matcher.end()));
-    }
-    return result;
+    return getMatchedRanges(patternInnerVars.matcher(text));
   }
 
+  private static final Pattern patternInnerEnvVars =
+      Pattern.compile(
+          "(?<="
+              + ENV_VAR_REF_BEGIN
+              + "|"
+              + ENV_VAR_BEGIN
+              + ")"
+              + VAR_NAME
+              + "(?="
+              + VAR_END
+              + ")");
   /**
    * Parse giving text to find inner ENV variables boundaries
+   *
    * @param text
    * @return list of inner variables boundaries NOT including $ENV{ or ENV{ and }
    */
   @NotNull
   public static List<TextRange> getInnerEnvVars(String text) {
-    List<TextRange> result = new ArrayList<>();
-    Pattern pattern = Pattern.compile(
-            "(?<="+ CMAKE_ENV_VAR_REF_BEGIN+"|"+CMAKE_ENV_VAR_BEGIN+")"+ CMAKE_VAR_NAME +"(?="+CMAKE_VAR_END+")");
-    Matcher matcher = pattern.matcher(text);
-    while (matcher.find()) {
-      result.add(new TextRange(matcher.start(), matcher.end()));
-    }
-    return result;
+    return getMatchedRanges(patternInnerEnvVars.matcher(text));
   }
 
+  private static List<TextRange> getMatchedRanges(@NotNull Matcher matcher) {
+    if (!matcher.find()) return EMPTY_RANGES_LIST;
+    List<TextRange> result = new ArrayList<>();
+    do {
+      result.add(new TextRange(matcher.start(), matcher.end()));
+    } while (matcher.find());
+    return result;
+  }
 }
