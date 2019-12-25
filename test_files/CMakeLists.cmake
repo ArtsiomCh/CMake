554f559f0a80362dc2eae86b7ff2b4337ddd5b0c
Artsiom Chapialiou <achapialiou@gmail.com>
2019-12-25 17:44:56 -0500

    193.2.1 release

--- test_files/CMakeLists.cmake
+++ test_files/CMakeLists.cmake
@@ -4,3 +4,5 @@ cmake_version(a 54544)
 [=[
     lklknllkn
 ]=]
+
+set_config_specific_property("OUTPUT_DIRECTORY" "${CMAKE_CURRENT_SOURCE_DIR}$<$<NOT:$<STREQUAL:${CMAKE_VS_PLATFORM_NAME},Win32>>:/${CMAKE_VS_PLATFORM_NAME}>/${PROPS_CONFIG}")
